f6c1fd8fc7083c7707bd946a35c47a80431003aa
Wemko Dijkhuis <wemko.dijkhuis@trivento.nl>
2018-04-18 21:16:06 +0200

    1.4 state and events

--- app/src/pages/GroceryListPage/GroceryListPage.js
+++ app/src/pages/GroceryListPage/GroceryListPage.js
@@ -2,8 +2,45 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 class GroceryListPage extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      groceries: props.groceries,
+      inputName: '',
+      inputAmount: ''
+    };
+
+    this.handleChangeName = this.handleChangeName.bind(this);
+    this.handleChangeAmount = this.handleChangeAmount.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChangeName(e) {
+    this.setState({
+      inputName: e.target.value
+    });
+  }
+
+  handleChangeAmount(e) {
+    this.setState({
+      inputAmount: e.target.value
+    });
+  }
+
+  handleSubmit() {
+    const { inputName, inputAmount, groceries } = this.state;
+
+    this.setState({
+      groceries: [...groceries, { name: inputName, amount: inputAmount }],
+      inputName: '',
+      inputAmount: ''
+    });
+  }
+
   render() {
-    const { title, groceries } = this.props;
+    const { title } = this.props;
+    const { groceries,inputAmount, inputName } = this.state;
 
     return (
       <div>
@@ -13,6 +50,9 @@ class GroceryListPage extends React.Component {
             <li key={index}>{grocery.name} {grocery.amount}</li>
           ))}
         </ul>
+        <input name="name" placeholder="name" value={inputName} onChange={this.handleChangeName} />
+        <input name="amount" type="number" value={inputAmount} placeholder="amount" onChange={this.handleChangeAmount} />
+        <button onClick={this.handleSubmit}>Submit</button>
       </div>
     );
   }
