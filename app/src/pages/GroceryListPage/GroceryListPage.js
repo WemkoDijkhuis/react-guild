a41e1eebd520aaedab05f2fbf90154d398536d58
Wemko Dijkhuis <wemko.dijkhuis@trivento.nl>
2018-04-18 20:54:17 +0200

    1.3 using props

--- app/src/pages/GroceryListPage/GroceryListPage.js
+++ app/src/pages/GroceryListPage/GroceryListPage.js
@@ -3,9 +3,16 @@ import PropTypes from 'prop-types';
 
 class GroceryListPage extends React.Component {
   render() {
+    const { title, groceries } = this.props;
+
     return (
       <div>
-          hello world
+        <h2>{title}</h2>
+        <ul>
+          {groceries.map((grocery, index) => (
+            <li key={index}>{grocery.name} {grocery.amount}</li>
+          ))}
+        </ul>
       </div>
     );
   }
@@ -20,6 +27,7 @@ GroceryListPage.propTypes = {
 };
 
 GroceryListPage.defaultProps = {
+  title: 'Grocery List',
   groceries: [
     {name: 'Cucumber', amount: 1},
     {name: 'Apples', amount: 3},
