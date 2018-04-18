bf42e97173bf9be361c1ba468527c2e27d0315da
Wemko Dijkhuis <wemko.dijkhuis@trivento.nl>
2018-04-18 20:32:38 +0200

    exercise 1.1 new component

---
+++ app/src/pages/GroceryListPage/GroceryListPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class GroceryListPage extends React.Component {
+  render() {
+    return (
+      <div>
+          hello world
+      </div>
+    );
+  }
+}
+
+export default GroceryListPage;
--- app/src/router.js
+++ app/src/router.js
@@ -2,11 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {browserHistory, IndexRoute, Route, Router} from 'react-router';
 
+import GroceryListPage from './pages/GroceryListPage/GroceryListPage';
+
 import Init from './main';
 
 ReactDOM.render(
   <Router history={browserHistory} >
     <Route path="/" component={Init} >
+        <IndexRoute component={GroceryListPage} />
     </Route>
   </Router>
   , document.querySelector('#init')
