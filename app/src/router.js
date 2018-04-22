44dcd0750e6f5cd548e01531e73de9431d6ca341
WemkoDijkhuis <wemko.dijkhuis@trivento.nl>
2018-04-22 21:47:53 +0200

    setup for redux

--- app/src/router.js
+++ app/src/router.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 import {browserHistory, IndexRoute, Route, Router} from 'react-router';
+import groceriesReducer from './reducers/groceries-reducer';
 
 import Init from './main';
 
+const initialStore = createStore(groceriesReducer, ['Use Redux']);
+
 ReactDOM.render(
-  <Router history={browserHistory} >
-    <Route path="/" component={Init} >
-    </Route>
-  </Router>
+  <Provider store={initialStore}>
+    <Router history={browserHistory} >
+      <Route path="/" component={Init} >
+      </Route>
+    </Router>
+  </Provider>
   , document.querySelector('#init')
 );
